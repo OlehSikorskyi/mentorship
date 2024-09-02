9e560d19ac47eace3a324e27a41daa585a0c5626
opylyp <kvitkakvitka321@gmail.com>
2024-09-02 16:58:07 +0300

    Java Programming Fail

--- src/test/java/tests/unit/SimpleBookManagerTest.java
+++ src/test/java/tests/unit/SimpleBookManagerTest.java
@@ -56,7 +56,7 @@ public class SimpleBookManagerTest {
         book = new EBook("Java Programming", "John Doeww", 29.99, Genre.SCIENCE, 5.5);
         manager.addBook(book);
         Book retrievedBook = manager.getBookDetails("Java Programming");
-        Assert.assertTrue(retrievedBook.getTitle().contains("Java Programming Fail"));
+        Assert.assertTrue(retrievedBook.getTitle().contains("Java Programmin"));
     }
 
     @AfterClass
